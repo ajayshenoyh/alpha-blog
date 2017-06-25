a920047657453c6ee495b9db45004b7bc51278ea
Ajay Shenoy <ajayshenoyh@gmail.com>
2017-06-25 03:11:52 +0000

    Create new template , action and routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'pages#homepage'
   get 'about_us', to: 'pages#about'
+  
+  resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
